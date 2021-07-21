92b6798e8fdd9ff3149a3b1d29cd14752e8e26a2
madlhawa <madhawa242@gmail.com>
2021-07-21 16:37:25 +0530

    q

--- solution/unloadS3.py
+++ solution/unloadS3.py
@@ -1,8 +1,19 @@
 import numpy as np
 from sqlalchemy import create_engine
 import pandas as pd
+from datetime import datetime
 
 engine = create_engine('postgresql://awsuser:Sysco123@redshift-db.c9adkqwvnbdx.ap-southeast-1.redshift.amazonaws.com:5439/rs_source')
-data_frame = pd.read_sql("unload('select * from customer') to 's3://seed-data-lake/customer/customer_' iam_role 'arn:aws:iam::340246275766:role/Redshift'csv; SELECT * FROM public.customer;", engine)
 
-print(data_frame)
+tables = [
+    'customer',
+    'orders',
+    'products',
+    'productcategories'
+]
+with engine.connect() as connection:
+    for table in tables:
+        query = f"unload('select * from {table}') to 's3://seed-data-lake/{table}/{str(datetime.now()).replace(' ','_')}/{table}_' iam_role 'arn:aws:iam::340246275766:role/Redshift'csv;"
+        result = connection.execute()
+
+print(result)
