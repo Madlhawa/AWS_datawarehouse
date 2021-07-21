d9d944d716a5c668d4f83478804ac042bd3a713d
madlhawa <madhawa242@gmail.com>
2021-07-21 16:19:14 +0530

    a

--- solution/connectRedshift.py
+++ solution/connectRedshift.py
@@ -1,22 +1,8 @@
-import psycopg2
 import numpy as np
 from sqlalchemy import create_engine
 import pandas as pd
-
-# con=psycopg2.connect(dbname= 'rs_source',
-#                     host='redshift-db.c9adkqwvnbdx.ap-southeast-1.redshift.amazonaws.com', 
-#                     port= '5439', 
-#                     user= 'awsuser', 
-#                     password= 'Sysco123')
-
-# cur = con.cursor()
-# cur.execute("SELECT * FROM `customer`;")
-# data = np.array(cur.fetchall())
 
 engine = create_engine('postgresql://awsuser:Sysco123@redshift-db.c9adkqwvnbdx.ap-southeast-1.redshift.amazonaws.com:5439/rs_source')
 data_frame = pd.read_sql('SELECT * FROM public.customer;', engine)
 
 print(data_frame)
-
-# cur.close() 
-# con.close()
--- solution/unloadS3.py
+++ solution/unloadS3.py
@@ -1,48 +1,8 @@
-############ REQUIREMENTS ####################
-# sudo apt-get install python-pip 
-# sudo apt-get install libpq-dev
-# sudo pip install psycopg2
-# sudo pip install sqlalchemy
-# sudo pip install sqlalchemy-redshift
-##############################################
-
-import sqlalchemy as sa
-from sqlalchemy.orm import sessionmaker
+import numpy as np
 from sqlalchemy import create_engine
+import pandas as pd
 
-#>>>>>>>> MAKE CHANGES HERE <<<<<<<<<<<<< 
-DATABASE = "rs_source"
-USER = "awsuser"
-PASSWORD = "Sysco123"
-HOST = "redshift-db.c9adkqwvnbdx.ap-southeast-1.redshift.amazonaws.com"
-PORT = "5439"
-SCHEMA = "public"      #default is "public" 
+engine = create_engine('postgresql://awsuser:Sysco123@redshift-db.c9adkqwvnbdx.ap-southeast-1.redshift.amazonaws.com:5439/rs_source')
+data_frame = pd.read_sql("unload('select * from customer') to 's3://seed-data-lake/customer/customer_' iam_role 'arn:aws:iam::340246275766:role/Redshift'csv; 'SELECT * FROM public.customer;", engine)
 
-####### connection and session creation ############## 
-connection_string = "redshift+psycopg2://%s:%s@%s:%s/%s" % (USER,PASSWORD,HOST,str(PORT),DATABASE)
-engine = create_engine(connection_string)
-session = sessionmaker()
-session.configure(bind=engine)
-s = session()
-SetPath = "SET search_path TO %s" % SCHEMA
-s.execute(SetPath)
-###### All Set Session created using provided schema  #######
-
-################ write queries from here ###################### 
-query = "unload('select * from customer') to 's3://seed-data-lake/unload/customer_' iam_role 'arn:aws:iam::340246275766:role/Ec2';"
-rr = s.execute(query)
-all_results =  rr.fetchall()
-
-def pretty(all_results):
-    for row in all_results :
-        print("row start >>>>>>>>>>>>>>>>>>>>")
-        for r in row :
-            print(" ----" + r)
-        print ("row end >>>>>>>>>>>>>>>>>>>>>>")
-
-
-pretty(all_results)
-
-
-########## close session in the end ###############
-s.close()
+print(data_frame)
