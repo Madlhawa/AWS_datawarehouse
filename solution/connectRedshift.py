976a3fba26ce75458e54df163bd739f2c3e6ddaf
madlhawa <madhawa242@gmail.com>
2021-07-21 15:59:36 +0530

    a

--- solution/connectRedshift.py
+++ solution/connectRedshift.py
@@ -14,7 +14,7 @@ import pandas as pd
 # data = np.array(cur.fetchall())
 
 engine = create_engine('postgresql://awsuser:Sysco123@redshift-db.c9adkqwvnbdx.ap-southeast-1.redshift.amazonaws.com:5439/rs_source')
-data_frame = pd.read_sql('SELECT * FROM `public.customer`;', engine)
+data_frame = pd.read_sql('SELECT * FROM public.customer;', engine)
 
 print(data_frame)
 
